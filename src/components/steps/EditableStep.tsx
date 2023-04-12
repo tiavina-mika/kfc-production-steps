1d3292b41af5a2fb5b99181fa43dbe3343be8854
Tiavina Michael Ralainirina <ralainirinatiavina@gmail.com>
2023-04-12 17:11:01 +0300

    refactor step preview in edition preview

--- src/components/steps/EditableStep.tsx
+++ src/components/steps/EditableStep.tsx
@@ -2,16 +2,7 @@ import React, { FC } from "react";
 
 import { Box } from "@mui/material";
 
-import { roundNumber } from "../../utils/utils";
-import { PRODUCTION_STEPS_COL_WIDTHS } from "../../utils/constant";
-import {
-  StyledStepBodyCell,
-  StyledStepFirstBodyColumn,
-  StyledStepText
-} from "../StyledSectionComponents";
-import StepNameDescription from "./StepNameDescription";
-
-const widths = PRODUCTION_STEPS_COL_WIDTHS;
+import StepPreview from "./StepPreview";
 
 type Props = {
   step: Record<string, any>;
@@ -59,50 +50,8 @@ const EditableStep: FC<Props> = ({
         display: "flex"
       }}
       onClick={_stopPropagation}
-      // className={`${isHover ? classes.editHover : ""} ${error || isDeleteHover ? classes.sectionLineError : ""} ${(step.parentId)?classes.sectionInherited:""}`}
     >
-      <StyledStepFirstBodyColumn className="flexRow center">
-        <StepNameDescription
-          name={step.name}
-          description={step.description}
-          index={index}
-        />
-      </StyledStepFirstBodyColumn>
-      <StyledStepBodyCell align="left" width={widths[1]}>
-        <StyledStepText>{step.inputWeight || "-"}</StyledStepText>
-      </StyledStepBodyCell>
-      <StyledStepBodyCell align="left" width={widths[2]}>
-        <StyledStepText>-</StyledStepText>
-      </StyledStepBodyCell>
-      <StyledStepBodyCell align="left" width={widths[3]}>
-        <StyledStepText>
-          {step.cost ? `${roundNumber(step.cost, 3)} €` : "_"}
-        </StyledStepText>
-      </StyledStepBodyCell>
-      <StyledStepBodyCell align="left" width={widths[4]}>
-        <StyledStepText>-</StyledStepText>
-      </StyledStepBodyCell>
-      <StyledStepBodyCell align="left" width={widths[5]}>
-        <StyledStepText>-</StyledStepText>
-      </StyledStepBodyCell>
-      <StyledStepBodyCell align="left" width={widths[6]}>
-        <StyledStepText>{step.outputWeight || "-"}</StyledStepText>
-      </StyledStepBodyCell>
-      <StyledStepBodyCell align="left" width={widths[7]}>
-        <StyledStepText>-</StyledStepText>
-      </StyledStepBodyCell>
-      <StyledStepBodyCell align="left" width={widths[8]}>
-        <StyledStepText>-</StyledStepText>
-      </StyledStepBodyCell>
-      <StyledStepBodyCell align="left" width={widths[9]}>
-        <StyledStepText>-</StyledStepText>
-      </StyledStepBodyCell>
-      <StyledStepBodyCell align="left" width={widths[10]}>
-        <StyledStepText>-</StyledStepText>
-      </StyledStepBodyCell>
-      <StyledStepBodyCell align="left" width={widths[11]}>
-        <StyledStepText>-</StyledStepText>
-      </StyledStepBodyCell>
+      <StepPreview step={step} index={index} />
     </Box>
   );
 };
